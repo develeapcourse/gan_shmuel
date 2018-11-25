6e6d81e8c53b3f6c7cd48cb5d6a54d9c31cb9dd4
lonny <lonnychetrit@gmail.com>
2018-11-25 16:22:01 +0200

    Infrastructure ready for the billing service

---
+++ bills/db/database_billing_service.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE flaskApp;
+use flaskApp;
+CREATE TABLE rates (
+                productName VARCHAR(400) NOT NULL,
+                scope VARCHAR(100) NOT NULL,
+                rates FLOAT NOT NULL,
+                PRIMARY KEY (productName, scope)
+);
+
+
+CREATE TABLE provider (
+                providerId INT NOT NULL,
+                providerName VARCHAR(400) NOT NULL,
+                PRIMARY KEY (providerId)
+);
+INSERT into provider VALUES (1,"Provider 1");
+
+CREATE TABLE truck (
+                truckId INT NOT NULL,
+                providerId INT NOT NULL,
+                PRIMARY KEY (truckId)
+);
+
+INSERT into truck VALUES (1,1);
+
+ALTER TABLE truck ADD CONSTRAINT provider_truck_fk
+FOREIGN KEY (providerId)
+REFERENCES provider (providerId)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION;
