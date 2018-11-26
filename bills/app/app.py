4238b3ca1f5c7239df40416b6d6afc53b1f4e60a
lonny <lonnychetrit@gmail.com>
2018-11-26 17:36:59 +0200

    Adding the PUT truck method

--- bills/app/app.py
+++ bills/app/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__, static_url_path='')
 databaseConfig = {
         'user': 'root',
         'password': 'root',
-        'host': 'db',
+        'host': 'billingservicedb',
         'port': '3306',
         'database': 'flaskApp'
     }
@@ -42,13 +42,65 @@ def providerUpdate(id):
     try:
         connection = mysql.connector.connect(**databaseConfig)
         cursor = connection.cursor()  
-        cursor.execute('UPDATE provider SET providerName = "{0}" WHERE providerId = {1}'.format("newName provider", 1))
+        cursor.execute('UPDATE provider SET providerName = "{0}" WHERE providerId = {1}'.format(request.form["providerName"], id))
         connection.commit()
         cursor.close()
         connection.close()
         return "ok"
     except Exception as e: 
-        return(e)
+        return(str(e))
+    
+
+@app.route('/provider', methods=["POST"])
+def providerinsert():
+    try:
+        connection = mysql.connector.connect(**databaseConfig)
+        cursor = connection.cursor()  
+        cursor.execute('Insert into provider VALUES (NULL, "{0}")'.format(request.form["providerName"]))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return "ok"
+    except Exception as e: 
+        return(str(e))
+
+
+@app.route('/truck/<id>?<path>')
+def getTruck(id):
+    url = 'http://example.com/?q=abc&p=123'
+    par = urlparse.parse_qs(urlparse.urlparse(url).query)
+
+    return str(par)
+    try:
+        r = requests.get('http://service_app_weight:5000/item/{0}'.format(id))
+        return r.json()
+    except Exception as e:
+        return str(e)
+
+
+@app.route('/truckList')
+def truckList():
+    connection = mysql.connector.connect(**databaseConfig)
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM truck')
+    results = [{truckId: providerId} for (truckId, providerId) in cursor]
+    cursor.close()
+    connection.close()
+    return str(results)
+
+    
+@app.route('/truck/<id>', methods=["POST"])
+def truckUpdate(id):
+    try:
+        connection = mysql.connector.connect(**databaseConfig)
+        cursor = connection.cursor()  
+        cursor.execute('UPDATE truck SET providerId = "{0}" WHERE truckId = {1}'.format(request.form["providerId"], id))
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return "ok"
+    except Exception as e: 
+        return(str(e))
     
 
     
