b5c3cc304c0e3f965cd11f2677e6bc0099fcd04b
maze88 <michael.52@msn.com>
2018-11-26 10:56:40 +0200

    made function skeletons for weighing service.

--- weight/app/app.py
+++ weight/app/app.py
@@ -1,6 +1,7 @@
 # -*-coding:utf-8 -*
 from typing import List, Dict
-from flask import Flask, request, json, jsonify
+from flask import Flask, request, jsonify
+import datetime
 import logging
 import mysql.connector
 import uuid
@@ -25,7 +26,7 @@ def init_config() -> List[Dict]:
     """
     config = {
     'user' : 'root',
-    'password' : 'root',
+    'pass  # temporary line, until function and return implementedword' : 'root',
     'host' : 'db',
     'port' : '3306',
     'database' : 'weight_system'
@@ -53,71 +54,110 @@ def csv_to_json(csvFile):
 def index() -> str:
     """
     for debugging purposes: dumps all database.
-
     """
     return json.dumps({'weight_system': init_config()})
 
-@app.route('/weight', methods = ['POST'])
-def post_weight(jsonData):
+@app.route('/weight?direction=<str:direction>&truck=<str:truck_id>&containers=<arr:container_ids>&weight=<int:weight>&unit=<str:unit>&force=<bool:force>&produce=<str:produce>', methods = ['POST'])
+def post_weight(direction, truck_id, container_ids, weight, unit, force, produce):
     """
     Records data and server date-time and returns a json object with a unique weight.
     Note that "in" & "none" will generate a new session id, and "out" will return session id of previous "in" for the truck.
+    Return json on success:
+    {
+      "id": <str>,
+      "truck": <license> or "na",
+      "bruto": <int>,
+      //ONLY for OUT:
+      "truckTara": <int>,
+      "neto": <int> or "na" // na if some of containers have unknown tara
+    }
     """
-    data = request.get_json()  # testing
-    print('printing YAY!!')  # testing
-    print(data)  # testing
-    return "returning YAY!!" + data  # testing
+    
+    # return json on success
+    pass  # temporary line, until function and return implemented
 
-@app.route('/batch-weight', methods = ['POST'])
-def post_batch_weight(jsonData):
+@app.route('/batch-weight?file=<str:filename>', methods = ['POST'])
+def post_batch_weight(filename):
     """
     Will upload list of tara weights from a file in "/in" folder. Usually used to accept a batch of new containers.
     File formats accepted: csv (id,kg), csv (id,lbs), json ([{"id":..,"weight":..,"unit":..},...])
     """
-    pass
+    with open('/in/{}'.format(filename), 'r') as f:
+        lines = f.readlines()
+    # do something with array `lines`
+    
+    # return something
+    pass  # temporary line, until function and return implemented
 
 @app.route('/unknown', methods = ['GET'])
-def get_unknown_containers(jsonData):
+def get_unknown_containers():
     """
     Returns a list of all recorded containers that have unknown weight:
     ["id1","id2",...]
     """
-    pass
+    
+    # return array of strings
+    pass  # temporary line, until function and return implemented
 
-@app.route('/weight', methods = ['GET'])  # /weight?from=t1&to=t2&filter=f
-def get_weight_from_file(jsonData):
+@app.route('/weight?from=<str:t1>&to=<str:t2>&filter=<str:filter>', methods = ['GET'])  # /weight?from=t1&to=t2&filter=f
+def get_weight_from_file(t1, t2, directions = ['in', 'out', 'none']):
     """
-    Will upload list of tara weights from a file in "/in" folder. Usually used to accept a batch of new containers.
-    File formats accepted: csv (id,kg), csv (id,lbs), json ([{"id":..,"weight":..,"unit":..},...])
+    - t1,t2 - date-time stamps, formatted as yyyymmddhhmmss. server time is assumed.
+    - directions - comma delimited list of directions. default is "in,out,none"
+    default t1 is "today at 000000". default t2 is "now".
+    returns an array of json objects, one per weighing (batch NOT included)
     """
-    pass
+    
+    # return array of json objects
+    pass  # temporary line, until function and return implemented
 
-@app.route('/item', methods = ['GET'])  # /item/<id>?from=t1&to=t2
-def get_item(jsonData):
+@app.route('/item/<str:id>?from=<str:t1>&to=<str:t2>', methods = ['GET'])  # /item/<id>?from=t1&to=t2
+def get_item(item_id, t1, t2):
     """
     - id is for an item (truck or container). 404 will be returned if non-existent
     - t1,t2 - date-time stamps, formatted as yyyymmddhhmmss. server time is assumed.
     default t1 is "1st of month at 000000". default t2 is "now".
+    Returns a json:
+    {
+      "id": <str>,
+      "tara": <int> OR "na", // for a truck this is the "last known tara"
+      "sessions": [ <id1>,...]
+    }
     """
+    
+    # return json
+    pass  # temporary line, until function and return implemented
 
-@app.route('/session', methods = ['GET'])  # /session/<id>
-def get_session_id(jsonData):
+@app.route('/session/<str:id>', methods = ['GET'])  # /session/<id>
+def get_session(session_id):
     """
-    id is for a weighing session. 404 will be returned if non-existent.
+    session_id is for a weighing session. 404 will be returned if non-existent.
+    Returns a json:
+    {
+      "id": <str>,
+      "truck": <truck-id> or "na",
+      "bruto": <int>,
+      //ONLY for OUT:
+      "truckTara": <int>,
+      "neto": <int> or "na" // na if some of containers unknown
+    }
     """
-    pass
+    
+    # return json
+    pass  # temporary line, until function and return implemented
 
 @app.route('/health', methods = ['GET'])
-def health(jsonData):
+def health():
     """
     health function...
     """
-    # test acess to database
     # test read from /in directory
+    # test acess to database
     # other tests...
     return "ok"
-    pass
+    pass  # temporary line, until function and return implemented
 
 
 if __name__ == '__main__':
+    logger.info('Starting Flask server...')
     app.run(host='0.0.0.0', debug=True, port=5000)
